b33294b275753825e1b9ac8bab086ded6248882c
EduGomes18 <edu_guto10@hotmail.com>
2020-05-03 09:44:37 -0300

    Finaliza tela de exposicao salao

--- src/Screens/App/CompanyStore/Screens/Schedule/index.js
+++ src/Screens/App/CompanyStore/Screens/Schedule/index.js
@@ -1,14 +1,169 @@
 import React from "react";
-import { View } from "react-native";
+import { StyleSheet, FlatList, ScrollView } from "react-native";
+import { LinearGradient } from "expo-linear-gradient";
 
-import { Container, Title } from "./styles";
+import { Entypo } from "@expo/vector-icons";
+import user1 from "~/Assets/Images/Userpic.png";
+import mani from "~/Assets/Images/man.jpg";
+import brush from "~/Assets/Images/brush.jpg";
+import pedi from "~/Assets/Images/ped.jpg";
+
+import user2 from "~/Assets/Images/Userpic.jpg";
+
+import {
+  Container,
+  Title,
+  RateArea,
+  RateCard,
+  DescribeText,
+  RateAvatar,
+  RateIcons,
+  RateStars,
+  CardInner,
+  DateDesc,
+  PromoCard,
+  PromoArea,
+  PromoPoints,
+  PromoDesc,
+  ServicesArea,
+  ServicesCircle,
+  ServPic,
+  ServDesc,
+  Services,
+} from "./styles";
+import elevationShadowStyle from "~/Components/ShadowFunc";
 
 const Schedule = () => {
+  const data = [
+    { id: "00", desc: "Adorei o servico! A Maria e uma fofa!", source: user2 },
+    { id: "01", desc: "boa profisional, adorei", source: user1 },
+    { id: "02", desc: "A maria e muito legal!", source: user1 },
+    { id: "03", desc: "Sempre tenho meu horario marcado...", source: user1 },
+  ];
+  const services = [
+    { id: "00", desc: "Manicure", source: mani },
+    { id: "01", desc: "Pedicure", source: pedi },
+    { id: "02", desc: "Escova", source: brush },
+    { id: "03", desc: "Manicure", source: mani },
+  ];
+
+  const promos = [
+    { id: "00", desc: "promocao manicure", points: 50 },
+    { id: "01", desc: "promocao escova", points: 62 },
+    { id: "02", desc: "promocao pedicure", points: 30 },
+    { id: "03", desc: "promocao manicure", points: 10 },
+  ];
+
   return (
     <Container>
-      <Title>Serviços</Title>
+      <ScrollView
+        contentContainerStyle={{
+          paddingBottom: 52,
+          paddingTop: 12,
+        }}
+      >
+        <Title>Avaliações</Title>
+        <RateArea>
+          <FlatList
+            showsHorizontalScrollIndicator={false}
+            horizontal
+            data={data}
+            keyExtractor={(item) => item.id}
+            renderItem={({ item }) => {
+              return (
+                <RateCard style={styles.shadow}>
+                  <CardInner>
+                    <RateAvatar resizeMode="cover" source={item.source} />
+                    <RateStars>
+                      <RateIcons>
+                        <Entypo name="star" color="#BADDA9" size={20} />
+                        <Entypo name="star" color="#BADDA9" size={20} />
+                        <Entypo name="star" color="#BADDA9" size={20} />
+                        <Entypo name="star" color="#BADDA9" size={20} />
+                        <Entypo name="star" color="#BADDA9" size={20} />
+                      </RateIcons>
+                      <DateDesc>ha 1 hora</DateDesc>
+                    </RateStars>
+                  </CardInner>
+
+                  <DescribeText>{item.desc}</DescribeText>
+                </RateCard>
+              );
+            }}
+          />
+        </RateArea>
+        <Title>Promoções</Title>
+        <PromoArea>
+          <FlatList
+            showsHorizontalScrollIndicator={false}
+            horizontal
+            data={promos}
+            keyExtractor={(item) => item.id}
+            renderItem={({ item }) => {
+              return (
+                <PromoCard style={styles.shadow}>
+                  <LinearGradient
+                    colors={["#FFA9A9", "transparent"]}
+                    start={[1, 1]}
+                    end={[0.8, 0]}
+                    style={{
+                      borderTopRightRadius: 12,
+                      borderBottomRightRadius: 12,
+                      position: "absolute",
+                      right: 0,
+                      width: "100%",
+                      height: "100%",
+                      top: 0,
+                    }}
+                  />
+                  <PromoDesc>{item.desc}</PromoDesc>
+                  <PromoPoints>₳ {item.points},00</PromoPoints>
+                </PromoCard>
+              );
+            }}
+          />
+        </PromoArea>
+        <Title>Serviços</Title>
+
+        <Services>
+          <FlatList
+            showsHorizontalScrollIndicator={false}
+            horizontal
+            data={services}
+            keyExtractor={(item) => item.id}
+            renderItem={({ item }) => {
+              return (
+                <ServicesArea>
+                  <ServicesCircle style={styles.shadow}>
+                    <ServPic source={item.source} />
+                  </ServicesCircle>
+                  <ServDesc>{item.desc}</ServDesc>
+                </ServicesArea>
+              );
+            }}
+          />
+        </Services>
+      </ScrollView>
     </Container>
   );
 };
 
 export default Schedule;
+
+const styles = StyleSheet.create({
+  shadow: {
+    ...elevationShadowStyle(6),
+    backgroundColor: "#fff",
+  },
+  shadowArea: {
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 11,
+    },
+    shadowOpacity: 0.55,
+    shadowRadius: 14.78,
+
+    elevation: 22,
+  },
+});
--- src/Screens/App/CompanyStore/Screens/Schedule/styles.js
+++ src/Screens/App/CompanyStore/Screens/Schedule/styles.js
@@ -1,6 +1,7 @@
 import styled from "styled-components/native";
-
-import { LightGray } from "~/Config/Global";
+import { Dimensions } from "react-native";
+import Constants from "expo-constants";
+import { Principal, Gray, Black, White, LightGray } from "~/Config/Global";
 
 export const Container = styled.View`
   flex: 1;
@@ -10,5 +11,112 @@ export const Container = styled.View`
 export const Title = styled.Text`
   font-size: 14px;
   font-weight: bold;
+  margin: 6px 12px;
   color: ${LightGray};
+`;
+
+export const RateCard = styled.View`
+  width: ${Dimensions.get("window").width / 2 + "px"};
+  height: ${Dimensions.get("window").width / 3 + "px"};
+  border-radius: 12px;
+  padding: 4px;
+  margin: 12px;
+  background: #fff;
+  justify-content: center;
+  align-items: center;
+`;
+
+export const RateArea = styled.View`
+  width: 100%;
+`;
+
+export const DescribeText = styled.Text`
+  font-size: 14px;
+  width: 90%;
+  margin-left: 6px;
+  height: 40px;
+  color: ${Black};
+`;
+
+export const DateDesc = styled.Text`
+  font-size: 12px;
+  color: ${LightGray};
+`;
+
+export const RateAvatar = styled.ImageBackground`
+  width: 60px;
+  height: 60px;
+`;
+
+export const RateStars = styled.View``;
+
+export const RateIcons = styled.View`
+  flex-direction: row;
+`;
+
+export const CardInner = styled.View`
+  width: 100%;
+  justify-content: space-around;
+  flex-direction: row;
+  height: 70px;
+  align-items: center;
+`;
+
+export const PromoCard = styled.View`
+  width: ${Dimensions.get("window").width / 1.8 + "px"};
+  height: ${Dimensions.get("window").width / 4.2 + "px"};
+  border-radius: 12px;
+  margin: 12px;
+  background: ${White};
+  flex-direction: row;
+  justify-content: space-around;
+  align-items: center;
+`;
+
+export const PromoArea = styled.View`
+  width: 100%;
+`;
+
+export const PromoPoints = styled.Text`
+  transform: rotate(-25deg);
+  margin-right: 6px;
+  margin-top: 6px;
+  font-size: 22px;
+  color: ${Principal};
+`;
+
+export const PromoDesc = styled.Text`
+  font-size: 14px;
+  width: 40%;
+  margin-left: 12px;
+  margin-bottom: 6px;
+  color: ${Black};
+`;
+
+export const ServicesArea = styled.View`
+  width: ${Dimensions.get("window").width / 3 + "px"};
+  height: ${Dimensions.get("window").width / 3 + "px"};
+  justify-content: space-around;
+  align-items: center;
+`;
+
+export const ServicesCircle = styled.View`
+  width: ${Dimensions.get("window").width / 4 + "px"};
+  height: ${Dimensions.get("window").width / 4 + "px"};
+  border-radius: 10000px;
+  justify-content: center;
+  align-items: center;
+`;
+
+export const ServPic = styled.Image`
+  border-radius: 1000px;
+`;
+
+export const ServDesc = styled.Text`
+  font-size: 14px;
+  color: ${LightGray};
+`;
+
+export const Services = styled.View`
+  width: 100%;
 `;
